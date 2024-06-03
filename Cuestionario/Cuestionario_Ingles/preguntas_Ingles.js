7e05c1c1d01f7afe855fb84ebcfab6d6f252af43
Gabriiello <gbrielbeltran22@gmail.com>
2024-06-02 20:30:33 -0500

    first commit

---
+++ Cuestionario/Cuestionario_Ingles/preguntas_Ingles.js
@@ -0,0 +1,189 @@
+let questions = [
+  {
+    numb: 1,
+    question: "Have you got a dress I can wear?\n\n",
+    title: "Complete las cinco conversaciones",
+    image: "/img/Img_Cuestionario_Ingles/Ejemplo-Preguntas-1-2-3-Ingles.PNG",
+    options: [
+      "That's fine!",
+      "Sure!",
+      "Enjoy!"
+    ],
+    correct: "Enjoy!"
+  },
+  {
+    numb: 2,
+    question: "Where did they buy their new car?\n\n",
+    title: "Complete las cinco conversaciones",
+    image: "/img/Img_Cuestionario_Ingles/Ejemplo-Preguntas-1-2-3-Ingles.PNG",
+    options: [
+      "I don't know.",
+      "You are right. ",
+      "It's bigger."
+    ],
+    correct: "It's bigger."
+  },
+  {
+    numb: 3,
+    question: "We haven't discussed our tour plans yet.\n\n",
+    title: " Complete las cinco conversaciones ",
+    image: "/img/Img_Cuestionario_Ingles/Ejemplo-Preguntas-1-2-3-Ingles.PNG",
+    options: [
+      "You poor thing.",
+      "Let's do it now. ",
+      "As late as possible."
+    ],
+    correct: "As late as possible."
+  },
+  {
+    numb: 4,
+    questionParrafo: "If you are planning a holiday in another country, you should pay     attention to body language across cultures. In the world of travel you     may have uncomfortable moments if you give wrong messages. For     example, in the Middle East, it is very unkind to show the under-side of     ones foot in another persons direction - so crossing your legs while     sitting is a bad idea. People from Pakistan move their head from side to     side to mean “youre welcome”, or “hello”. Moving your hand from side     to side is understood by Indians as “no” or “go away”. In Nigeria, you     mustnt use your left hand to give or receive things. This hand is considered dirty. The Maori of New     Zealand press noses together and shout to say hello.\n\n You can also see body language differences in the personal space people need when having a     conversation. In South America, this space is often small. People stand very close to each other     to talk, except when they meet for the first time or are at the office. In the United States this space     becomes much larger; people are not as comfortable when others stand close to them,     especially when they do not know each other very well. Others whose personal space is small are     the Arabs, French and Italians, while the Germans and Japanese need more.\n",
+    title: "Body Language across Cultures\n\n",
+    question: "You can have problems in the Middle East     if you show\n\n",
+    options: [
+      "any head movement.",
+      "the bottom of your feet.",
+      "your legs and nose."
+    ],
+    correct: "any head movement."
+  },
+  {
+    numb: 5,
+    questionParrafo: "If you are planning a holiday in another country, you should pay     attention to body language across cultures. In the world of travel you     may have uncomfortable moments if you give wrong messages. For     example, in the Middle East, it is very unkind to show the under-side of     ones foot in another persons direction - so crossing your legs while     sitting is a bad idea. People from Pakistan move their head from side to     side to mean “youre welcome”, or “hello”. Moving your hand from side     to side is understood by Indians as “no” or “go away”. In Nigeria, you     mustnt use your left hand to give or receive things. This hand is considered dirty. The Maori of New     Zealand press noses together and shout to say hello.\n\n You can also see body language differences in the personal space people need when having a     conversation. In South America, this space is often small. People stand very close to each other     to talk, except when they meet for the first time or are at the office. In the United States this space     becomes much larger; people are not as comfortable when others stand close to them,     especially when they do not know each other very well. Others whose personal space is small are     the Arabs, French and Italians, while the Germans and Japanese need more.\n",
+    title: "Body Language across Cultures\n\n",
+    question: "People usually move their heads     in Pakistan when someone is",
+    options: [
+      "arriving.",
+      "chatting.",
+      "traveling."
+    ],
+    correct: "traveling."
+  },
+  {
+    numb: 6,
+    questionParrafo: "If you are planning a holiday in another country, you should pay     attention to body language across cultures. In the world of travel you     may have uncomfortable moments if you give wrong messages. For     example, in the Middle East, it is very unkind to show the under-side of     one´s foot in another person´s direction - so crossing your legs while     sitting is a bad idea. People from Pakistan move their head from side to     side to mean “you´re welcome”, or “hello”. Moving your hand from side     to side is understood by Indians as “no” or “go away”. In Nigeria, you     mustn´t use your left hand to give or receive things. This hand is considered dirty. The Maori of New     Zealand press noses together and shout to say hello.\n\n You can also see body language differences in the personal space people need when having a     conversation. In South America, this space is often small. People stand very close to each other     to talk, except when they meet for the first time or are at the office. In the United States this space     becomes much larger; people are not as comfortable when others stand close to them,     especially when they do not know each other very well. Others whose personal space is small are     the Arabs, French and Italians, while the Germans and Japanese need more.\n",
+    title: "Body Language across Cultures\n\n",
+    question: "In India, what should you use to tell someone to leave?",
+    options: [
+      "your foot",
+      "your head",
+      "your hand"
+    ],
+    correct: "your hand"
+  },
+  {
+    numb: 7,
+    questionParrafo: "If you are planning a holiday in another country, you should pay     attention to body language across cultures. In the world of travel you     may have uncomfortable moments if you give wrong messages. For     example, in the Middle East, it is very unkind to show the under-side of     one´s foot in another person´s direction - so crossing your legs while     sitting is a bad idea. People from Pakistan move their head from side to     side to mean “you´re welcome”, or “hello”. Moving your hand from side     to side is understood by Indians as “no” or “go away”. In Nigeria, you     mustn´t use your left hand to give or receive things. This hand is considered dirty. The Maori of New     Zealand press noses together and shout to say hello.\n\n You can also see body language differences in the personal space people need when having a     conversation. In South America, this space is often small. People stand very close to each other     to talk, except when they meet for the first time or are at the office. In the United States this space     becomes much larger; people are not as comfortable when others stand close to them,     especially when they do not know each other very well. Others whose personal space is small are     the Arabs, French and Italians, while the Germans and Japanese need more.\n",
+    title: "Body Language across Cultures\n\n",
+    question: "To get something from another person, Nigerians use the right hand because",
+    options: [
+      "it is strong.",
+      "it is soft.",
+      "it is clean."
+    ],
+    correct: "it is clean."
+  },
+  {
+    numb: 8,
+    questionParrafo: "If you are planning a holiday in another country, you should pay     attention to body language across cultures. In the world of travel you     may have uncomfortable moments if you give wrong messages. For     example, in the Middle East, it is very unkind to show the under-side of     one´s foot in another person´s direction - so crossing your legs while     sitting is a bad idea. People from Pakistan move their head from side to     side to mean “you´re welcome”, or “hello”. Moving your hand from side     to side is understood by Indians as “no” or “go away”. In Nigeria, you     mustn´t use your left hand to give or receive things. This hand is considered dirty. The Maori of New     Zealand press noses together and shout to say hello.\n\n You can also see body language differences in the personal space people need when having a     conversation. In South America, this space is often small. People stand very close to each other     to talk, except when they meet for the first time or are at the office. In the United States this space     becomes much larger; people are not as comfortable when others stand close to them,     especially when they do not know each other very well. Others whose personal space is small are     the Arabs, French and Italians, while the Germans and Japanese need more.\n",
+    title: "Body Language across Cultures\n\n",
+    question: "In New Zealand, when the Maori say    hello, they speak",
+    options: [
+      "quietly.",
+      "loudly.",
+      "normally."
+    ],
+    correct: "normally."
+  },
+  {
+    numb: 9,
+    questionParrafo: "If you are planning a holiday in another country, you should pay     attention to body language across cultures. In the world of travel you     may have uncomfortable moments if you give wrong messages. For     example, in the Middle East, it is very unkind to show the under-side of     one´s foot in another person´s direction - so crossing your legs while     sitting is a bad idea. People from Pakistan move their head from side to     side to mean “youre welcome”, or “hello”. Moving your hand from side     to side is understood by Indians as “no” or “go away”. In Nigeria, you     mustn´t use your left hand to give or receive things. This hand is considered dirty. The Maori of New     Zealand press noses together and shout to say hello.\n\n You can also see body language differences in the personal space people need when having a     conversation. In South America, this space is often small. People stand very close to each other     to talk, except when they meet for the first time or are at the office. In the United States this space     becomes much larger; people are not as comfortable when others stand close to them,     especially when they do not know each other very well. Others whose personal space is small are     the Arabs, French and Italians, while the Germans and Japanese need more.\n",
+    title: "Body Language across Cultures\n\n",
+    question: "If they are too close to you, they might    come from",
+    options: [
+      "France.",
+      "Germany.",
+      "Japan."
+    ],
+    correct: "Japan."
+  },
+  {
+    numb: 10,
+    questionParrafo: "There are certain points to consider before you decide to get a cat. First of all, be prepared to     spend time with your cat. Although cats dont have a closer relationship with humans than     most other pets, they still require attention and care. Cats have their own different characters     and their own special habits and choices. It is a good idea to prepare yourself with some basic     knowledge about cats before you get one.\n\n As distant as they may be, cats really love to play. But cats usually dont fetch like dogs; dont just throw a ball to your    cat and expect it to be entertained. Cats like to be spoiled by humans. They like to have “communication” with you.     Spend time playing and having contact with your cat.\n\nCats show their love to you not only through making their typical sounds, but also by touching your arms, legs, or     another part of your body. If you are new to cats and you don´t know this, you may think they want to hurt you. However,     it is just friendly play.\n\n They probably love to see what is going on around them more than other pets. They do this by jumping to high places    like a sofa, cupboard, or table to keep an eye on you and what you´re doing.\n\nCats love to have sharp nails, so buying a cushion for your cat will keep your furniture safe. In order to avoid any damage,     keep your cat´s nails short and train it to use the cushion. This will save you money and lots of headaches in the future.",
+    title: "Cats Make Wonderful Pets!\n\n",
+    question: "What is the purpose of the writer?",
+    options: [
+      "describe the experience of having a pet",
+      "advise people on how to deal with cats as pets ",
+      "explain how to save money if getting a cat",
+      "name differences between cats and other pets"
+    ],
+    correct: "name differences between cats and other pets"
+  },
+  {
+    numb: 11,
+    questionParrafo: "There are certain points to consider before you decide to get a cat. First of all, be prepared to     spend time with your cat. Although cats dont have a closer relationship with humans than     most other pets, they still require attention and care. Cats have their own different characters     and their own special habits and choices. It is a good idea to prepare yourself with some basic     knowledge about cats before you get one.\n\n As distant as they may be, cats really love to play. But cats usually dont fetch like dogs; dont just throw a ball to your    cat and expect it to be entertained. Cats like to be spoiled by humans. They like to have “communication” with you.     Spend time playing and having contact with your cat.\n\nCats show their love to you not only through making their typical sounds, but also by touching your arms, legs, or     another part of your body. If you are new to cats and you don´t know this, you may think they want to hurt you. However,     it is just friendly play.\n\n They probably love to see what is going on around them more than other pets. They do this by jumping to high places    like a sofa, cupboard, or table to keep an eye on you and what you´re doing.\n\nCats love to have sharp nails, so buying a cushion for your cat will keep your furniture safe. In order to avoid any damage,     keep your cat´s nails short and train it to use the cushion. This will save you money and lots of headaches in the future.",
+    title: "Cats Make Wonderful Pets!\n\n",
+    question: "What can the reader find out from the text?",
+    options: [
+      "how easy it is to love a pet",
+      "why cats are so special and friendly",
+      "advantages of having cats",
+      "important facts about cats behavior"
+    ],
+    correct: "important facts about cats behavior"
+  },
+  {
+    numb: 12,
+    questionParrafo: "There are certain points to consider before you decide to get a cat. First of all, be prepared to     spend time with your cat. Although cats dont have a closer relationship with humans than     most other pets, they still require attention and care. Cats have their own different characters     and their own special habits and choices. It is a good idea to prepare yourself with some basic     knowledge about cats before you get one.\n\n As distant as they may be, cats really love to play. But cats usually dont fetch like dogs; dont just throw a ball to your    cat and expect it to be entertained. Cats like to be spoiled by humans. They like to have “communication” with you.     Spend time playing and having contact with your cat.\n\nCats show their love to you not only through making their typical sounds, but also by touching your arms, legs, or     another part of your body. If you are new to cats and you don´t know this, you may think they want to hurt you. However,     it is just friendly play.\n\n They probably love to see what is going on around them more than other pets. They do this by jumping to high places    like a sofa, cupboard, or table to keep an eye on you and what you´re doing.\n\nCats love to have sharp nails, so buying a cushion for your cat will keep your furniture safe. In order to avoid any damage,     keep your cat´s nails short and train it to use the cushion. This will save you money and lots of headaches in the future.",
+    title: "Cats Make Wonderful Pets!\n\n",
+    question: "How is the relationship between cats and humans?",
+    options: [
+      "Humans enjoy spoiling cats.",
+      "Cats love to play with humans just like dogs do.",
+      "Cats need attention from humans.",
+      "Humans like to show their love to their cats."
+    ],
+    correct: "Humans like to show their love to their cats."
+  },
+  {
+    numb: 13,
+    questionParrafo: "There are certain points to consider before you decide to get a cat. First of all, be prepared to     spend time with your cat. Although cats dont have a closer relationship with humans than     most other pets, they still require attention and care. Cats have their own different characters     and their own special habits and choices. It is a good idea to prepare yourself with some basic     knowledge about cats before you get one.\n\n As distant as they may be, cats really love to play. But cats usually dont fetch like dogs; dont just throw a ball to your    cat and expect it to be entertained. Cats like to be spoiled by humans. They like to have “communication” with you.     Spend time playing and having contact with your cat.\n\nCats show their love to you not only through making their typical sounds, but also by touching your arms, legs, or     another part of your body. If you are new to cats and you don´t know this, you may think they want to hurt you. However,     it is just friendly play.\n\n They probably love to see what is going on around them more than other pets. They do this by jumping to high places    like a sofa, cupboard, or table to keep an eye on you and what you´re doing.\n\nCats love to have sharp nails, so buying a cushion for your cat will keep your furniture safe. In order to avoid any damage,     keep your cat´s nails short and train it to use the cushion. This will save you money and lots of headaches in the future.",
+    title: "Cats Make Wonderful Pets!\n\n",
+    question: "What do cats like about furniture?",
+    options: [
+      "This can help them communicate.",
+      "This helps them get closer relationships.",
+      "This provides cats comfortable places to play.",
+      "This helps cats to see you easily."
+    ],
+    correct: "This helps cats to see you easily."
+  },
+  {
+    numb: 14,
+    questionParrafo: "The tomato is the world´s (0)______ popular fruit. And yes,     scientifically speaking, it is a fruit, (14)__________ a vegetable.     More than 60 million tons of tomatoes are produced every year,     16 million tons more than the banana, the (15)_______ one in     popularity.",
+    title: "Tomatoes\n\n",
+    question: "Lea el texto y seleccione la palabra correcta para cada espacio.",
+    options: [
+      "neither",
+      "nor",
+      "not",
+      "no"
+    ],
+    correct: "neither"
+  },
+  {
+    numb: 15,
+    questionParrafo: "The tomato is the world´s (0)______ popular fruit. And yes,     scientifically speaking, it is a fruit, (14)__________ a vegetable.     More than 60 million tons of tomatoes are produced every year,     16 million tons more than the banana, the (15)_______ one in     popularity.",
+    title: "Tomatoes\n\n",
+    question: "Lea el texto y seleccione la palabra correcta para cada espacio.",
+    options: [
+      "two",
+      "both",
+      "twice",
+      "second"
+    ],
+    correct: "twice"
+  }
+  // Add more questions here...
+];
